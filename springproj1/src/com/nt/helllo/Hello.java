a84ccfe5a7ca551c20ee757f04f8d7026852a926
HOME <HOME@DESKTOP-5SKJAR1>
2018-06-13 00:13:14 +0530

    ap

---
+++ springproj1/src/com/nt/helllo/Hello.java
@@ -0,0 +1,13 @@
+package com.nt.helllo;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("HI UD");
+		System.out.println("HI UD");
+		System.out.println("HI UD");
+		System.out.println("HI UD");
+
+	}
+
+}
